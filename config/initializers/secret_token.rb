dd6824146b8d60c6871692da618b7a5e2cbbfecc
radhika.p <radhika.p@sybrant.com>
2012-11-26 14:52:42 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zatfish::Application.config.secret_token = 'ba36ba5e87690522bed04e2e211384d199b21bd6db0df1f0d79557ea0508a7fd4425737b6efede9fce61d72403b98694f3831f315c7658d92b68e3b4d4947b8a'
