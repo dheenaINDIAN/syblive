08da9cff9b0c9aa86f6f924db7a2503f497cba09
radhika.p <radhika.p@sybrant.com>
2012-11-29 20:25:00 +0530

    production delivery error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Zatfish::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
+config.action_mailer.raise_delivery_errors = true
   config.assets.debug = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
