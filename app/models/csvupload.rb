7f9b01b0c43bf26c25ddb793c2bd49a0414dd274
dheena <dheenaindian@gmail.com>
2013-01-03 11:12:09 +0530

    change

--- app/models/csvupload.rb
+++ app/models/csvupload.rb
@@ -1,5 +1,5 @@
 class Csvupload < ActiveRecord::Base
    attr_accessible :csvfile ,:category
-   has_attached_file :csvfile, :url => "/assets/file/:id/:basename.:extension",
-                             :path => ":rails_root/public/assets/file/:id/:basename.:extension"
+   has_attached_file :csvfile, :url => "app/assets/fold/:id/:basename.:extension",
+                             :path => ":rails_root/app/assets/fold/:id/:basename.:extension"
 end
