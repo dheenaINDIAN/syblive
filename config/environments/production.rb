91e52cb035c5a301c969b1f30e103ceee354d015
dheena <dheenaindian@gmail.com>
2013-01-03 12:21:33 +0530

    produ change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Zatfish::Application.configure do
   config.action_controller.perform_caching = true
   config.action_mailer.default_url_options = { :host => 'dev-zatfish.herokuapp.com' }
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
