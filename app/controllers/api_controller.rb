3b9ddb71ca57734b34b863dcdc1a5a72f81e7e1d
dheena <dheenaindian@gmail.com>
2013-01-04 15:26:08 +0530

    kk

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -51,8 +51,17 @@ def category
       user = User.find_by_email(login)
       if user && user.valid_password?(password)
       @csvreports = Csvupload.where("category = ?", params[:category])
-	  @csvreport = @csvreports.map{|x| x.category = x.csvfile.url}
-	   respond_to do |format|
+	  @favour = Favourate.where(:type_fav => params[:category],:user_fav => user.id).map(&:file_fav)
+	    if @favour != []
+		@list = []
+	    @favour.map{|x| @list << Csvupload.where(:id => x)}
+		@list.flatten!
+		@diff = @csvreports - @list
+		@csvreport = @list.map{|x| x.csvfile_content_type = 'favourite'}
+		@csvreports = @diff.concat(@list)
+		end
+		@reportfinal = @csvreports.map{|x| x.category = x.csvfile.url}
+		 respond_to do |format|
        format.json   { render :json => @csvreports }
        end
 	  else
@@ -71,34 +80,23 @@ def category
 end
 
 def new
- # if params[:login] && params[:credential]
-      # login = params[:login]
-      # password = params[:credential]
-      # user = User.find_by_email(login)
-      # if user && user.valid_password?(password)
-      # @csvreports = Csvupload.order("created_at DESC").limit(10)
-	   # respond_to do |format|
-       # format.json   { render :json => @csvreports }
-       # end
-	  # else
-	   # respond_to do |format|
-	   # @csvreports = 'invalid'
-       # format.json   { render :json => @csvreports  }
-	   # end
-	  # end
- # else  
-	# respond_to do |format|
-	# @csvreports = 'invalid'
-    # format.json   { render :json => @csvreports  }  
-    # end	  
- # end
  if params[:login] && params[:credential] && params[:category]
       login = params[:login]
       password = params[:credential]
       user = User.find_by_email(login)
       if user && user.valid_password?(password)
       @csvreports = Csvupload.where("category = ?", params[:category]).order("created_at DESC").limit(5)
-	   respond_to do |format|
+	  @favour = Favourate.where(:type_fav => params[:category],:user_fav => user.id).map(&:file_fav)
+	    if @favour
+		@list = []
+	    @favour.map{|x| @list << Csvupload.where(:id => x)}
+		@list.flatten!
+		@diff = @csvreports - @list
+		@csvreport = @list.map{|x| x.csvfile_content_type = 'favourite'}
+		@csvreports = @diff.concat(@list)
+		end
+		@reportfinal = @csvreports.map{|x| x.category = x.csvfile.url}
+		 respond_to do |format|
        format.json   { render :json => @csvreports }
        end
 	  else
@@ -116,11 +114,73 @@ def new
  p "-------------#{@csvreports}-------"
 end
 
+def favourite
+ if params[:login] && params[:credential] && params[:file] && params[:type]
+      login = params[:login]
+      password = params[:credential]
+      user = User.find_by_email(login)
+      if user && user.valid_password?(password)
+	   check = Favourate.where(:user_fav =>user.id,:file_fav => params[:file],:type_fav => params[:type])
+	   p "kk--#{params[:type]}---kkk  fff#{check}"
+	   if check == []
+	    @favourite = Favourate.create(:user_fav =>user.id,:file_fav => params[:file],:type_fav => params[:type])
+	    respond_to do |format|
+        format.json   { render :json => "success" }
+        end
+	   else
+	    respond_to do |format|
+        format.json   { render :json => "already exit or type is invalid" }
+        end
+	   end
+	  else
+	   respond_to do |format|
+	   @csvreports = 'invalid'
+       format.json   { render :json => @csvreports  }
+	   end
+	  end
+ else  
+	respond_to do |format|
+	@csvreports = 'invalid'
+    format.json   { render :json => @csvreports  }  
+    end	  
+ end
+ 
+end
 
 
-
-
-
+def favourite_list
+ if params[:login] && params[:credential] && params[:type]
+      login = params[:login]
+      password = params[:credential]
+      user = User.find_by_email(login)
+      if user && user.valid_password?(password)
+	   @fileid = Favourate.where(:type_fav => params[:type],:user_fav => user.id).map(&:file_fav)
+	   p "------#{@fileid.inspect}---"
+	   if @fileid == []
+	    respond_to do |format|
+        format.json   { render :json => "no favourite" }
+        end
+	   else
+	    @list = []
+	    @fileid.map{|x| @list << Csvupload.where(:id => x)}
+	    respond_to do |format|
+        format.json   { render :json => @list.flatten! }
+        end
+	   end
+	  else
+	   respond_to do |format|
+	   @csvreports = 'invalid'
+       format.json   { render :json => @csvreports  }
+	   end
+	  end
+ else  
+	respond_to do |format|
+	@csvreports = 'invalid'
+    format.json   { render :json => @csvreports  }  
+    end	  
+ end
+ 
+end
 
 
  
